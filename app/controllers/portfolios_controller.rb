2f5306b6e86805f06fbc9fd998062f2f42bd3238
Ruben Teixeira <rubteixeira@deloitte.pt>
2020-12-25 12:17:30 +0000

    Added new and create actions in Portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,26 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @new_portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio was successfully created!' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
