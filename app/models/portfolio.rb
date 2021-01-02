eb4916a94cae5c0e2de4f044e0ac3f3e4212b6a9
Ruben Teixeira <rubteixeira@deloitte.pt>
2021-01-02 17:49:07 +0000

    Implemented images defaults for portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,12 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails, -> { where(title: 'ruby on rails') }
   scope :angular, -> { where(title: 'angular') }
+
+  after_initialize :set_defaults
+
+  # Helper function to set default images values for portfolios without one
+  def set_defaults
+    self.main_image ||= 'https://place-hold.it/600x200'
+    self.thumb_image ||= 'https://place-hold.it/350x200'
+  end
 end
