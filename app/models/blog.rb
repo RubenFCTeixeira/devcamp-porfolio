6972fab7d6231a44a16781dda067f95c1ff47ec7
Ruben Teixeira <rubteixeira@deloitte.pt>
2021-01-02 16:40:33 +0000

    Created Topic model and stablish data relations with Blogs model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,8 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  belongs_to :topic
+
   enum status: { draft: 0, published: 1 }
 
   validates_presence_of :title, :body
