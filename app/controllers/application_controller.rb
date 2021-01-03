dd67d8c5d4bdb60fc092feb9abf343e3ca812f99
Ruben Teixeira <rubteixeira@deloitte.pt>
2021-01-03 21:28:22 +0000

    Implemented null object design pattern for Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
 
   include DeviseWhitelist
   include SetSource
+
+  def current_user
+    super || OpenStruct.new(name: 'Guest User', first_name: 'Guest', last_name: 'User', email: 'guest@fake.com')
+  end
 end
