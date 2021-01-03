936efea854dcffb5be7b9f2697d0abf3699bc171
Ruben Teixeira <rubteixeira@deloitte.pt>
2021-01-03 15:39:07 +0000

    Added virtual attributes for Username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name
+
+  # Helper function to create virtual attribute (first name) for a given Username
+  def first_name
+    name.split.first
+  end
+
+  # Helper function to create virtual attribute (last name) for a given Username
+  def last_name
+    name.split.last
+  end
 end
